65e53b5b5e3d6fbcc79ed5b527613932f166c31c
Ngoc Duy <99950593+ngduy2506@users.noreply.github.com>
2022-09-14 20:53:31 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,12 +1,3 @@
-# Tặng Crush
-## _Một điều nho nhỏ tỏ tình với crush_
-
-Liên lạc: 
-[![Facebook](https://i.imgur.com/GRqy96ts.jpg)](https://www.facebook.com/nam.nodemy)
-[![Tiktok](https://i.imgur.com/Nbfl1E7t.jpg)](https://www.tiktok.com/@manindev)
-
-Mở file config.js sửa nội dung theo mẫu
-```
 const CONFIG = {
     introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
@@ -22,9 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay 7h anh qua đón nhé Cô Vee nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
-```
-
